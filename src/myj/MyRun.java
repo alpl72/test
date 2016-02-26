b4bc8ac71112cbe83d23a90fbfcf2aa644f94aec
haohao <haohao@qq.com>
2016-02-26 23:06:48 +0800

    myfirst-git

---
+++ src/myj/MyRun.java
@@ -0,0 +1,13 @@
+package myj;
+
+public class MyRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+
+}
